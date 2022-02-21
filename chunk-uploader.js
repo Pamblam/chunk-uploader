3b06766c66780436244e333a1fd95df6187f152a
pamblam <adelphia@protonmail.ch>
2022-02-21 10:31:51 -0500

    inital commit

---
+++ chunk-uploader.js
@@ -0,0 +1,26 @@
+
+class ChunkUploader{
+	
+	constructor(file, url, chunk_size){
+		this.file = file;
+		this.url = url;
+		this.chunk_size = chunk_size || 1000 * 1024;
+		this.reader = new FileReader();
+	}
+	
+	start(){
+		
+	}
+	
+	_upload_chunk(start){
+		var next_chunk = start + this.chunk_size + 1;
+		var blob = this.file.slice(start, next_chunk);
+		var blob_size = blob.size;
+		this.reader.onloadend = function(event){
+			if(event.target.readyState !== FileReader.DONE) return;
+			
+		};
+		this.reader.readAsDataURL(blob);
+	}
+	
+}
